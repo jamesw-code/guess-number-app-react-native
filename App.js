73a5de686992b1428670be2ed22ee7204b856d81
James Wyatt <jamesrwyatt35@gmail.com>
2024-05-06 20:21:24 -0400

    step one

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!!!!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <StartGameScreen />
   );
 }
 
