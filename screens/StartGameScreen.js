248a3d7e78214a5a58863e261a35241c647148ad
James Wyatt <jamesrwyatt35@gmail.com>
2024-05-08 01:12:59 -0400

    input field

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,7 +4,7 @@ import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen() {
     return(
         <View style={styles.inputContainer}>
-            <TextInput />
+            <TextInput style={styles.numberInput} maxLength={2} />
             <PrimaryButton>Reset</PrimaryButton>
             <PrimaryButton>Confirm</PrimaryButton>
         </View>
@@ -16,8 +16,25 @@ export default StartGameScreen;
 
 const styles = StyleSheet.create({
     inputContainer: {
-        flex: 1,
         padding: 16,
         marginTop: 100,
+        marginHorizontal: 24,
+        borderRadius: 8,
+        elevation: 10,
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2},
+        shadowRadius: 6,
+        backgroundColor: '#82063c'
+    },
+    numberInput: {
+        height: 50,
+        width: 50,
+        textAlign: 'center',
+        fontSize: 32,
+        borderBottomColor: '#ddb52f',
+        borderBottomWidth: 2,
+        color: '#ddb52f',
+        marginVertical: 8,
+        fontWeight: 'bold'
     }
 })
