f7fce715cb363bd66812fec066e56fabf133bb19
James Wyatt <jamesrwyatt35@gmail.com>
2024-05-08 01:51:57 -0400

    background image

--- App.js
+++ App.js
@@ -1,18 +1,28 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, ImageBackground } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
-    <View style={styles.rootScreen}>
-      <StartGameScreen />
-    </View>
+    <LinearGradient colors={['#85063c','#ddb52f']} style={styles.rootScreen}>
+      <ImageBackground 
+        source={require('./assets/images/beach.jpg')}
+        resizeMode='cover'
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+        >
+        <StartGameScreen />
+      </ImageBackground>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
-    backgroundColor: '#ddb52f',
   },
+  backgroundImage: {
+    opacity: 0.15
+  }
 });
