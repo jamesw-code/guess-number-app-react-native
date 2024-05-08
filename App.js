044c1311e82d432bc78a0ae5281e90976f5c2df2
James Wyatt <jamesrwyatt35@gmail.com>
2024-05-08 01:39:20 -0400

    background color

--- App.js
+++ App.js
@@ -1,18 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
-    <StartGameScreen />
+    <View style={styles.rootScreen}>
+      <StartGameScreen />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  rootScreen: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#ddb52f',
   },
 });
