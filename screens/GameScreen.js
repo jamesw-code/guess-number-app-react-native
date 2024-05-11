8b67c7e39440acddc015d65f5295bd8cd083f6ea
James Wyatt <jamesrwyatt35@gmail.com>
2024-05-11 15:09:54 -0400

    next guess logic

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,8 +1,9 @@
 import { useState } from "react";
-import {View, Text, StyleSheet } from "react-native";
+import {View, Text, StyleSheet, Alert } from "react-native";
 
 import Title from "../components/ui/title";
 import NumberContainer from "../components/game/NumberContainer";
+import PrimaryButton from "../components/ui/PrimaryButton";
 
 function generateRandomBetween(min, max, exclude) {
   const rndNum = Math.floor(Math.random() * (max - min)) + min;
@@ -14,10 +15,31 @@ function generateRandomBetween(min, max, exclude) {
   }
 }
 
+let minBoundary = 1;
+let maxBoundary = 100;
+
 function GameScreen({userNumber}) {
-  const initialGuess = generateRandomBetween(1, 100, userNumber);
+  const initialGuess = generateRandomBetween(minBoundary, maxBoundary, userNumber);
   const [currentGuess, setCurrentGuess] = useState(initialGuess);
 
+  function nextGuessHandler(direction) {
+    console.log(currentGuess + ' '+ userNumber)
+    if(
+      (direction === 'lower' && currentGuess < userNumber) ||
+      (direction === 'greater' && currentGuess > userNumber)
+    ) {
+      Alert.alert("Are you sure?", "Was that correct?", [{text: 'Sorry', style: 'cancel'}])
+      return;
+    }
+
+    if(direction === 'lower'){
+      maxBoundary = currentGuess;
+    } else {
+      minBoundary = currentGuess + 1; 
+    }
+    const newRndNumber = generateRandomBetween(minBoundary, maxBoundary, currentGuess);
+    setCurrentGuess(newRndNumber);
+  }
 
   return (
     <View style={styles.screen}>
@@ -25,6 +47,10 @@ function GameScreen({userNumber}) {
       <NumberContainer>{initialGuess}</NumberContainer>
       <View>
         <Text>Higher or Lower?</Text>
+        <View>
+          <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>-</PrimaryButton>
+          <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')}>+</PrimaryButton>
+        </View>
       </View>
     </View>
   );
